6486849c1f7e5d263a93a4485e59914ef915ef39
Jonathan Greene <jon@greenethumb.com>
2020-04-14 12:55:40 -0500

    app header border color endarkening

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 const StyledHeader = styled.div`
     padding: 2em;
-    border-bottom: 1px dashed #999;
+    border-bottom: 1px dashed #555;
     display: inline-flex;
 `;
 
